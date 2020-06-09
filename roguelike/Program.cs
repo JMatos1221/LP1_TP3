cfe3a823a8c1911671a912f1d141b7fade74093e
João Matos <joao.matos1221@gmail.com>
2020-06-09 16:03:30 +0100

    PrintMenu method added

--- roguelike/Program.cs
+++ roguelike/Program.cs
@@ -4,9 +4,25 @@ namespace roguelike
 {
     class Program
     {
+        /// <summary>
+        /// Main function, the program starts here
+        /// </summary>
+        /// <param name="args">Command lines arguments</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+
+        /// <summary>
+        /// Prints the Game Menu
+        /// </summary>
+        private void PrintMenu()
+        {
+            Console.WriteLine("1. New game");
+            Console.WriteLine("2. High scores");
+            Console.WriteLine("3. Instructions");
+            Console.WriteLine("4. Credits");
+            Console.WriteLine("5. Quit");
         }
     }
 }
